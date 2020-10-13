516daf2b3a11c9e215148ee2697ea2d5166e8b9a
b18042 <e1b18042@oit.ac.jp>
2020-10-13 15:29:36 +0900

    リポジトリの初期化

---
+++ src/test/java/oit/is/lec03_4/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec03_4.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
